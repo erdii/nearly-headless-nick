ff16dca1b6247cc1a628143a5ddc2d5e9d04b851
erdii <erdii@users.noreply.github.com>
2017-09-01 16:37:52 +0200

    started inline docs

--- src/lib.ts
+++ src/lib.ts
@@ -9,10 +9,30 @@ import * as Errors from "./errors";
 
 const log = createLogger("lib");
 
+/**
+ * hash("input")
+ *
+ * create sha256 hash of input string
+ *
+ * @export
+ * @param {string} input string to hash
+ * @returns {string} sha256 hash of input string
+ */
 export function hash(input: string) {
 	return crypto.createHash("sha256").update(input).digest("hex");
 }
 
+/**
+ * sanitizeUrl("www.example.com")
+ *
+ * sanitizes and validates a url by parsing it, and checking if the proto is either http: or https:
+ * defaults proto to http: and throws if proto is entirely unknown
+ *
+ * @export
+ * @param {string} pageUrl the url to sanitize
+ * @returns {string} sanitized url
+ * @throws {ProtocolError} if the url protocol is unknown
+ */
 export function sanitizeUrl(pageUrl: string) {
 	if (!pageUrl) throw new Errors.MissingUrlError();
 
@@ -22,17 +42,28 @@ export function sanitizeUrl(pageUrl: string) {
 		if (parsedUrl.protocol) {
 			throw new Errors.ProtocolError(pageUrl, parsedUrl.protocol);
 		} else {
-			return "http://" + pageUrl;
+			parsedUrl.protocol = "http:";
+			return url.format(parsedUrl);
 		}
 	}
 
 	return pageUrl;
 }
 
+/**
+ * sanitizePosInt("120")
+ *
+ * ensures that it returns a positive integer (no float)
+ * returns null if the parsed number does not equal the input string
+ *
+ * @export
+ * @param {string} intString number string to sanitize and parse
+ * @returns {number} a guaranteed positive integer
+ */
 export function sanitizePosInt(intString: string): number {
 	const parsed = parseInt(intString, 10);
 
-	// bail out if the number does not equal the textual representation
+	// bail out if the number does not equal input string
 	if (parsed.toString() !== intString) {
 		return null;
 	}
@@ -43,8 +74,17 @@ export function sanitizePosInt(intString: string): number {
 		: parsed;
 }
 
+/**
+ * createOptions(req.query)
+ *
+ * creates a screenshot options object from an express req.query object
+ *
+ * @export
+ * @param {*} query an express req.query object
+ * @returns IScreenshotOpts
+ */
 export function createOptions(query: any) {
-	const { w, h, sw, sh, fp } = query;
+	const { w, h, sw, sh, fp, nojs } = query;
 
 	const options = {} as IScreenshotOpts;
 
@@ -53,6 +93,7 @@ export function createOptions(query: any) {
 	if (sw != null) options.targetWidth = sanitizePosInt(sw);
 	if (sh != null) options.targetHeight = sanitizePosInt(sh);
 	if (fp != null) options.fullPage = true;
+	if (nojs != null) options.noJs = true;
 
 	return options;
 }
