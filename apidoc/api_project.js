e20968dfe85cfbbafaa418cb08ecfdb0b486962a
Joshua Gwosdz <erdiicodes@gmail.com>
2017-09-04 20:35:23 +0200

    apidoc update

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,14 +1,14 @@
 define({
   "title": "Nearly Headless Nick API Documentation",
   "name": "nearly-headless-nick",
-  "version": "0.0.4",
+  "version": "0.1.0",
   "description": "",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-09-02T13:52:05.261Z",
+    "time": "2017-09-04T18:35:22.920Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
